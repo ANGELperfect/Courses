3f9881a12fce9434a7a5086d6dfd1df1685fd83f
ANGELperfect <62509819+ANGELperfect@users.noreply.github.com>
2020-04-09 10:09:46 +0100

    Create Courses.js

---
+++ Courses.js
@@ -0,0 +1,90 @@
+function fr() {
+	document.getElementById('study1').innerHTML="Emplois";
+	document.getElementById('study2').innerHTML="Cours";
+	document.getElementById('study3').innerHTML="Examains";
+	document.getElementById('study4').innerHTML="Support";
+	document.getElementById('study').innerHTML="Etude";
+	document.getElementById('useful').innerHTML="Utile";
+	document.getElementById('useful1').innerHTML="Regles";
+	document.getElementById('useful2').innerHTML="Liens";
+	document.getElementById('useful3').innerHTML="Citations";
+	document.getElementById('useful4').innerHTML="Conseils";
+	document.getElementById('tools').innerHTML="Outils";
+	document.getElementById('tools1').innerHTML="Calculatrice";
+	document.getElementById('others').innerHTML="Autres";
+	document.getElementById('others1').innerHTML="Salut";
+	document.getElementById('intro').innerHTML="Introduction au domaine";
+	document.getElementById('intro1').innerHTML="faire connaissance de quoi vous allez etudier";
+	document.getElementById('s1').innerHTML="Semestre01";
+	document.getElementById('s2').innerHTML="Semestre02";
+	document.getElementById('s3').innerHTML="Semestre03";
+	document.getElementById('s4').innerHTML="Semestre04";
+	document.getElementById('SCIs5').innerHTML="Semestre05";
+	document.getElementById('SCIs6').innerHTML="Semestre06";
+	document.getElementById('SIs5').innerHTML="Semestre05";
+	document.getElementById('SIs6').innerHTML="Semestre06";
+	document.getElementById('TIs5').innerHTML="Semestre05";
+	document.getElementById('TIs6').innerHTML="Semestre06";
+	document.getElementById('GLs5').innerHTML="Semestre05";
+	document.getElementById('GLs6').innerHTML="Semestre06";
+	document.getElementById('l3').innerHTML="Ici quelques informations sur les specialites";
+	document.getElementById('links').innerHTML="Liens externes";
+	document.getElementById('linksp').innerHTML="Quelques liens pour vous aider";
+	document.getElementById('contact').innerHTML="Contacter nous";
+	document.getElementById('help').innerHTML="Aide";
+	document.getElementById('bugs').innerHTML="Problemes";
+	document.getElementById('rate').innerHTML="Evaluer";
+	document.getElementById('search').innerHTML="Rechercher ici";
+	document.getElementById('search').setAttribute('placeholder',"rechercher ici");
+
+
+
+}
+
+function eng() {
+	document.getElementById('study1').innerHTML="Schedules";
+	document.getElementById('study2').innerHTML="Courses";
+	document.getElementById('study3').innerHTML="Exams";
+	document.getElementById('study4').innerHTML="Support";
+	document.getElementById('study').innerHTML="Study";
+	document.getElementById('useful').innerHTML="Useful";
+	document.getElementById('useful1').innerHTML="Law";
+	document.getElementById('useful2').innerHTML="Links";
+	document.getElementById('useful3').innerHTML="Quoates";
+	document.getElementById('useful4').innerHTML="Advices";
+	document.getElementById('tools').innerHTML="Tools";
+	document.getElementById('tools1').innerHTML="Calculator	";
+	document.getElementById('others').innerHTML="Others";
+	document.getElementById('others1').innerHTML="Hi";
+	document.getElementById('intro').innerHTML="Introduction into domaine";
+	document.getElementById('intro1').innerHTML="Know what are you going to study";
+	document.getElementById('s1').innerHTML="Semester01";
+	document.getElementById('s2').innerHTML="Semester02";
+	document.getElementById('s3').innerHTML="Semester03";
+	document.getElementById('s4').innerHTML="Semester04";
+	document.getElementById('SCIs5').innerHTML="Semester05";
+	document.getElementById('SCIs6').innerHTML="Semester06";
+	document.getElementById('SIs5').innerHTML="Semester05";
+	document.getElementById('SIs6').innerHTML="Semester06";
+	document.getElementById('TIs5').innerHTML="Semester05";
+	document.getElementById('TIs6').innerHTML="Semester06";
+	document.getElementById('GLs5').innerHTML="Semester05";
+	document.getElementById('GLs6').innerHTML="Semester06";
+	document.getElementById('l3').innerHTML="Here is some informations about specialties";
+	document.getElementById('links').innerHTML="External links";
+	document.getElementById('linksp').innerHTML="Check here some useful links";
+	document.getElementById('contact').innerHTML="Contact us";
+	document.getElementById('help').innerHTML="Help";
+	document.getElementById('bugs').innerHTML="Bugs";
+	document.getElementById('rate').innerHTML="Rate us";
+	document.getElementById('search').innerHTML="Search here";
+	document.getElementById('search').setAttribute('placeholder',"search here");
+}
+
+function Dark() {
+	document.getElementById('style').setAttribute('href',"CoursesDark.css");
+}
+
+function Light() {
+	document.getElementById('style').setAttribute('href',"Courses.css");
+}
